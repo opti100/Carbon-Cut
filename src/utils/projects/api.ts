31acd936490a3373653b539ebf5d1f28d5e8c075
Rishi Patel <rishi@optiminastic.com>
2025-11-10 14:10:26 +0530

    fix: updated base url

--- src/utils/projects/api.ts
+++ src/utils/projects/api.ts
@@ -53,7 +53,7 @@ export interface ProjectsListResponse {
     total_pages: number;
   };
 }
-const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/inventory';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/v1/inventory';
 
 export const fetchAllProjects = async (): Promise<ProjectDTO[]> => {
   try {
