697121e8a8dcbe54d5cc917f9a697ab4310f3bdf
nitin <tech2@carboncut.co>
2025-12-17 14:55:05 +0530

    update form

--- src/components/adopters/form.tsx
+++ src/components/adopters/form.tsx
@@ -15,7 +15,7 @@ export default function FormSection() {
       />
 
       <div className="w-full h-screen overflow-hidden">
-       <iframe data-tally-src="https://tally.so/r/MeX9y0?transparentBackground=1" width="100%" height="100%" marginHeight={0} marginWidth={0} title="CarbonCut - Website/Application Onboarding" style={{ border: 0 }} />
+       <iframe  data-tally-src="https://tally.so/r/kd9b5e?transparentBackground=1" width="100%" height="100%" marginHeight={0} marginWidth={0} title="CarbonCut - Website/Application Onboarding" style={{ border: 0 }} />
       </div>
     </>
   );
