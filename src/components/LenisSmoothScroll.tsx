19e683fd590223147db01b8a7466119b17d52303
Rishi Patel <rishipatel0826@gmail.com>
2025-12-23 11:05:19 +0530

    feat: added lenis smooth scroll

---
+++ src/components/LenisSmoothScroll.tsx
@@ -0,0 +1,51 @@
+"use client";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useEffect, useRef } from "react";
+import { ReactLenis } from "lenis/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+interface LenisSmoothScrollProps {
+  children: React.ReactNode;
+}
+
+const LenisSmoothScroll: React.FC<LenisSmoothScrollProps> = ({ children }) => {
+  const lenisRef = useRef<any>(null);
+
+  useEffect(() => {
+    function update(time: number) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    ScrollTrigger.refresh();
+
+    return () => {
+      gsap.ticker.remove(update);
+    };
+  }, []);
+
+  return (
+    <ReactLenis
+      root
+      options={{
+        autoRaf: false,
+        duration: 4,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        orientation: "vertical",
+        gestureOrientation: "vertical",
+        smoothWheel: true,
+        wheelMultiplier: 1,
+        touchMultiplier: 2,
+        infinite: false,
+      }}
+      ref={lenisRef}
+    >
+      {children}
+    </ReactLenis>
+  );
+};
+
+export default LenisSmoothScroll;
