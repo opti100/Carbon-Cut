767e19ed8b7f466ea452c97dc15ba05dffccaabc
Rishi Patel <rishi@optiminastic.com>
2026-03-04 10:47:53 +0530

    feat: added microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://www.googletagmanager.com/gtag/js?id=G-Y388SH4Y4G"
           strategy="afterInteractive"
         />
+        <Script id="clarity-analytics" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vqcachazxh");
+          `}
+        </Script>
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
